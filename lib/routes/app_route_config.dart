3fb274e666416dfb26ec0b96bdf227cf0a749c0e
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-08-15 21:36:57 +0600

    routes constants

--- lib/routes/app_route_config.dart
+++ lib/routes/app_route_config.dart
@@ -5,28 +5,29 @@ import 'package:portfolio_website/pages/not_found_page.dart';
 import 'package:portfolio_website/pages/projects.dart';
 import 'package:portfolio_website/pages/blogs.dart';
 import 'package:portfolio_website/pages/home_page.dart';
+import 'package:portfolio_website/routes/app_route_constants.dart';
 
 class AppRoutes {
   GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
-        name: 'home',
+        name: Routes.homeRoute,
         pageBuilder: (context, state) => const MaterialPage(child: HomePage()),
       ),
       GoRoute(
         path: '/about',
-        name: 'about',
+        name: Routes.aboutRoute,
         pageBuilder: (context, state) => const MaterialPage(child: AboutPage()),
       ),
       GoRoute(
         path: '/blogs',
-        name: 'blogs',
+        name: Routes.blogsRoute,
         pageBuilder: (context, state) => const MaterialPage(child: BlogPage()),
       ),
       GoRoute(
         path: '/projects',
-        name: 'projects',
+        name: Routes.projectsRoute,
         pageBuilder: (context, state) => const MaterialPage(child: Projects()),
       ),
     ],
---
+++ lib/routes/app_route_constants.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String homeRoute = 'home';
+  static const String aboutRoute = 'about';
+  static const String blogsRoute = 'blogs';
+  static const String projectsRoute = 'projects';
+}
